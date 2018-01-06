551eaaddac260a5d614703560087c5bb34c5e2ae
Lars Møllebjerg <lars@moellebjerg.com>
2018-01-06 21:55:36 +0000

    Address can now be specified by command line argument or environment variable.

--- src/gnyfio.cpp
+++ src/gnyfio.cpp
@@ -5,9 +5,6 @@
 #include <grpc++/server_context.h>
 #include <grpc++/security/server_credentials.h>
 
-
-
-
 #include "../build/gen/gnyfio.grpc.pb.h"
 // If wiringPi is included before the grpc includes, the compile will fail.
 // I do not want to know why at this moment... moving on :)
@@ -33,11 +30,36 @@ class GnyfioServiceImplementation final : public GnyfioService::Service
 	
 };
 
+std::string getCmdOption(int argc, char **argv, const std::string& option, const std::string &defaultValue)
+{
+	std::string cmd;
+	std::string pattern = "-" + option + "=";
+	for (int i = 0; i < argc; i++)
+	{
+		std::string arg = argv[i];
+		if (0 == arg.find(pattern))
+		{
+			cmd = arg.substr(pattern.length());
+			return cmd;
+		}
+	} 
+	std::string envName = "GNYFIO_" + option;
+	
+	std::transform(envName.begin(), envName.end(),envName.begin(), ::toupper);
+	
+	if (const char* env = std::getenv(envName.c_str()))
+	{
+		return env;
+	}
+	
+	return defaultValue;
+}
+
 int main(int argc, char **argv)
 {
 	wiringPiSetup () ;
 	// TODO: Allow specifying port and IP address binding through command line and environment variable.
-	std::string server_address("0.0.0.0:50051");
+	std::string server_address = getCmdOption(argc, argv, "address", "0.0.0.0:50051");
 	
 	GnyfioServiceImplementation service;
 	
